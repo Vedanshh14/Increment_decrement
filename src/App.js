303d68217622f380a516107692b6f7c88943ab41
“Vedansh <vedanshupadhyay1401@gmail.com>
2024-09-04 23:56:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count,setCount] = useState(0);
+
+  function decreaseHandler(){
+    setCount(count-1);
+
+  }
+  function increaseHandler(){
+    setCount(count+1);
+
+  }
+  function resetHandler(){
+    setCount(0);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className=" flex flex-col justify-center items-center bg-[#344151] w-[100vw] h-[100vh]">
+      <div className="my-10 text-cyan-200 text-4xl">
+        Increment and Decrement
+      </div>
+
+      <div className="flex bg-white rounded ">
+      <button onClick={decreaseHandler} className="mx-4 my-2 text-xl">-</button>
+      <div className="my-2 mx-4 text-xl">{count}</div>
+      <button onClick={increaseHandler} className="mx-4 my-2 text-xl">+</button>
+      </div>
+
+      <div className="bg-cyan-200 py-2 px-5 rounded my-10">
+        <button onClick={resetHandler} >Reset</button>
+      </div>
     </div>
+    
+  
   );
 }
 
